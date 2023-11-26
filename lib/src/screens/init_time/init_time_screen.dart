1cd0cec5c1b9a069676129a2b3a3d54d946bfbef
Churchill Atchedji <churchill.atchedji@limpidius.com>
2023-11-26 16:11:35 +0100

    refactor(init_time_screen): refactor init time screen view
    The buttons used for init time screen have a certain time latency before apply there method on onPressed, so fix it.

--- lib/src/screens/init_time/init_time_screen.dart
+++ lib/src/screens/init_time/init_time_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:stud_advice/src/common/chore/app_colors.dart';
-import 'package:stud_advice/src/screens/init_time/widgets/init_time_connection_button.dart';
 import 'package:stud_advice/src/screens/login/login_screen.dart';
 import 'package:stud_advice/src/screens/register/register_options_screen.dart';
+import 'package:stud_advice/src/widgets/buttons/default_connection_button.dart';
 
 class InitTimeScreen extends StatelessWidget {
   static const String navigatorId = '/init_time_screen';
@@ -13,19 +13,33 @@ class InitTimeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Service.
-
     return Scaffold(
       backgroundColor: AppColors.white,
       body: SafeArea(
-        child: OrientationBuilder(
-          builder: (context, orientation) {
-            if (orientation == Orientation.portrait) {
-              return buildPortraitLayout(context);
-            } else {
-              return buildLandscapeLayout(context);
-            }
-          },
+        child: ListView(
+          children: [
+            SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+            buildImage(
+              imagePath: 'assets/images/common/raven_image.png',
+              height: 235,
+              width: 235,
+              margin: const EdgeInsets.only(top: 20),
+            ),
+            buildConnectionButton(
+              text: registerText,
+              textColor: AppColors.white,
+              onPressed: () {
+                Get.to(() => RegisterOptionsScreen());
+              },
+            ),
+            buildConnectionButton(
+              text: loginText,
+              textColor: AppColors.white,
+              onPressed: () {
+                Get.to(() => LoginScreen());
+              },
+            ),
+          ],
         ),
       ),
     );
@@ -34,20 +48,15 @@ class InitTimeScreen extends StatelessWidget {
   Widget buildConnectionButton({
     required String text,
     required Color textColor,
-    required VoidCallback onTap,
-    required EdgeInsets margin,
+    required VoidCallback onPressed,
   }) {
-    return Positioned(
-      bottom: margin.bottom,
-      left: margin.left,
-      right: margin.right,
-      child: InitTimeConnectionButton(
-        text: text,
-        textColor: textColor,
-        backgroundColor: AppColors.blue,
-        borderColor: AppColors.secondaryColor,
-        onTap: onTap,
-      ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: DefaultConnectionButton(
+          text: text,
+          textColor: textColor,
+          backgroundColor: AppColors.blue,
+          onPressed: onPressed),
     );
   }
 
@@ -57,73 +66,13 @@ class InitTimeScreen extends StatelessWidget {
     double? width,
     EdgeInsets? margin,
   }) {
-    return Positioned(
-      bottom: margin?.bottom,
-      left: margin?.left,
-      right: margin?.right,
+    return Padding(
+      padding: margin ?? EdgeInsets.zero,
       child: Image(
         image: AssetImage(imagePath),
         height: height,
         width: width,
       ),
-    );
-  }
-
-  Widget buildPortraitLayout(BuildContext context) {
-    return Stack(
-      children: [
-        buildConnectionButton(
-          text: registerText,
-          textColor: AppColors.white,
-          onTap: () {
-            Get.to(() => RegisterOptionsScreen());
-          },
-          margin: const EdgeInsets.only(bottom: 342, left: 50, right: 50),
-        ),
-        buildImage(
-          imagePath: 'assets/images/common/raven_image.png',
-          height: 235,
-          width: 235,
-          margin: const EdgeInsets.only(bottom: 370, left: 60, right: 80),
-        ),
-        buildConnectionButton(
-          text: loginText,
-          textColor: AppColors.white,
-          onTap: () {
-            Get.to(() => LoginScreen());
-          },
-          margin: const EdgeInsets.only(bottom: 250, left: 50, right: 50),
-        ),
-      ],
-    );
-  }
-
-  Widget buildLandscapeLayout(BuildContext context) {
-    return Stack(
-      children: [
-        buildConnectionButton(
-          text: registerText,
-          textColor: AppColors.white,
-          onTap: () {
-            Get.to(() => RegisterOptionsScreen());
-          },
-          margin: const EdgeInsets.only(bottom: 110, left: 250, right: 250),
-        ),
-        buildImage(
-          imagePath: 'assets/images/common/raven_image.png',
-          height: 170,
-          width: 170,
-          margin: const EdgeInsets.only(bottom: 148, left: 60, right: 80),
-        ),
-        buildConnectionButton(
-          text: loginText,
-          textColor: AppColors.white,
-          onTap: () {
-            Get.to(() => LoginScreen());
-          },
-          margin: const EdgeInsets.only(bottom: 20, left: 250, right: 250),
-        ),
-      ],
     );
   }
 }
