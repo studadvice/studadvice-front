8a23b889f414766a309faf5f6247866089bcdb3a
Churchill Atchedji <churchill.atchedji@limpidius.com>
2023-11-26 20:40:56 +0100

    feat(facebook): add facebook sign in process (android only)

--- lib/src/controller/authentication/authentication_controller.dart
+++ lib/src/controller/authentication/authentication_controller.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:stud_advice/src/common/conf/app_dependencies_binding.dart';
@@ -53,6 +54,26 @@ class AuthenticationController extends GetxController {
 
     if (userCredentials.user != null) {
       return true;
+    }
+    return false;
+  }
+
+  Future<bool> signInWithFacebook() async {
+    // Trigger the sign-in flow.
+    final LoginResult loginResult = await FacebookAuth.instance.login();
+
+    // Create a credential from the access token.
+    if (loginResult.accessToken != null) {
+      final OAuthCredential facebookAuthCredential =
+          FacebookAuthProvider.credential(loginResult.accessToken!.token);
+
+      // Signed in.
+      final UserCredential userCredentials = await FirebaseAuth.instance
+          .signInWithCredential(facebookAuthCredential);
+
+      if (userCredentials.user != null) {
+        return true;
+      }
     }
     return false;
   }
