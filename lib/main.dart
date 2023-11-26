1eaa6c83af4a357e4f029c12900af5371e96e156
Churchill Atchedji <churchill.atchedji@limpidius.com>
2023-11-26 11:27:28 +0100

    imp(connectivity): improve detect connectivity status process

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,62 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:country_picker/country_picker.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
-import 'package:stud_advice/src/common/chore/styles.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:stud_advice/src/common/chore/app_colors.dart';
+import 'package:stud_advice/src/common/chore/supported_locales.dart';
+import 'package:stud_advice/src/common/conf/app_dependencies_binding.dart';
 
 import 'firebase_options.dart';
 import 'src/common/conf/routes_configuration.dart';
-
-// import 'package:stud_advice/src/common/conf/injection_container.dart';
-
-import 'package:stud_advice/src/controller/common/i18n_controller.dart';
-import 'package:stud_advice/src/controller/common/theme_controller.dart';
+import 'src/controller/connectivity/connectivity_controller.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  // setupDependenciesInjection();
-  final ThemeController themeController = Get.put(ThemeController());
-  final I18n i18n = Get.put(I18n());
-  final RoutesConfiguration routesConfiguration = Get.put(RoutesConfiguration());
-
-  runApp( StudAdviceApp(
-          themeController: themeController,
-          i18n: i18n,
-          routesConfiguration: routesConfiguration));
+  runApp(const StudAdviceApp());
 }
 
 class StudAdviceApp extends StatelessWidget {
-  final ThemeController themeController;
-  final I18n i18n;
-  final RoutesConfiguration routesConfiguration;
-
-  StudAdviceApp(
-      {required this.themeController,
-      required this.i18n,
-      required this.routesConfiguration});
+  const StudAdviceApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp.router(
-          title: "Stud'Advice",
-          theme: Styles.lightTheme,
-          darkTheme: Styles.darkTheme,
-          themeMode: themeController.getThemeMode(),
-          debugShowCheckedModeBanner: false,
-          getPages: routesConfiguration.configureRoutes(),
-          localizationsDelegates: [
-            GlobalMaterialLocalizations.delegate,
-            CountryLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-          ],
-          locale: i18n.getCurrentLocale,
-          supportedLocales: [
-            const Locale('en', 'US'),
-            const Locale('fr', 'FR')
-          ],
-        );
+    final RoutesConfiguration routesConfiguration =
+        Get.put(RoutesConfiguration());
+
+    final ConnectivityController connectivityController =
+        Get.put(ConnectivityController());
+
+    Connectivity().onConnectivityChanged.listen((ConnectivityResult result) {
+      connectivityController.checkConnectivity(result);
+    });
+
+    return GetMaterialApp(
+      title: "Stud'Advice",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.white),
+        useMaterial3: true,
+        textTheme: GoogleFonts.latoTextTheme(
+          // Use Lato as default text style according to the graphic charter.
+          Theme.of(context).textTheme,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: RoutesConfiguration.initialRoute,
+      initialBinding: AppDependenciesBinding(),
+      getPages: routesConfiguration.configureRoutes(),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        CountryLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [const Locale(english), const Locale(french)],
+    );
   }
 }
