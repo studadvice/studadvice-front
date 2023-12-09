85dad15c8a09bb14fabe5caa722b9951e85a7579
amevigbe <yaoisidore.amevigbe.etu@univ-lille.fr>
2023-12-09 03:52:57 +0100

    feat(roadmap) 🚀: Méthode de translation
    Ajout d'une méthode qui réalise la translation d'un path 🖋️
    
    DEVSTUD-39 💻

--- lib/src/widgets/common/paints/curved_dottedline/curved_dottedline_paint.dart
+++ lib/src/widgets/common/paints/curved_dottedline/curved_dottedline_paint.dart
@@ -1,4 +1,6 @@
 import 'dart:core';
+import 'dart:typed_data';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:path_drawing/path_drawing.dart';
@@ -22,31 +24,40 @@ class CurvedDottedLinePainter extends CustomPainter {
       ..strokeWidth = strokeWidth;
 
     Path path = Path();
+    path = _buildCurvePathIfIsRight(path, size);
 
     if (isLeft) {
-      path = _buildCurvePathIfIsLeft(path, size);
-    } else {
-      path = _buildCurvePathIfIsRight(path, size);
+      path = _buildMirrorCurvePath(path, size);
     }
 
     canvas.drawPath(
         dashPath(path, dashArray: CircularIntervalList([10, 9.5])), paint);
   }
 
-  _buildCurvePathIfIsLeft(Path path, Size size) {
-    // Commencez à partir du côté droit au lieu du côté gauche
-    path.moveTo(size.width, size.height / 50);
+  Path _buildMirrorCurvePath(Path originalPath, Size size) {
+    // Find the ending point of the original curve
+    PathMetric pathMetric = originalPath.computeMetrics().last;
+    Tangent? tangent = pathMetric.getTangentForOffset(pathMetric.length);
+    Offset endPoint = tangent!.position;
 
-    // Ajoutez la courbe de Bézier cubique en inversant les coordonnées X
-    path.cubicTo(
-        size.height + (size.height * (1 / 3)), size.width - (size.width * (0.7 / 2)),
-      size.width - (size.width * (2 / 3)), size.height - (size.height * (1.497 / 2.044)),
-      0, size.height,
-    );
+    // Print x and y coordinates of the end point
+    print('endPoint.dx: ${endPoint.dx} endPoint.dy: ${endPoint.dy}');
 
-    return path;
+    // Create a mirrored path by applying a matrix transformation
+    Path mirrorPath = Path.from(originalPath);
+
+    final Matrix4 matrix = Matrix4.identity()
+      ..translate(0.0, endPoint.dy) // Translate the path down to the x-axis
+      ..scale(1.0, -1.0) // Mirror scale on the y-axis
+      ..translate(0.0, -endPoint.dy); // Translate the path back up by the same amount
+
+    mirrorPath = mirrorPath.transform(matrix.storage);
+
+    // Translate the entire path down by the y-coordinate of the endpoint to start from the same y-position
+    mirrorPath = mirrorPath.shift(const Offset(1, 1));
+
+    return mirrorPath;
   }
-
 
   _buildCurvePathIfIsRight(Path path, Size size) {
       path.moveTo(20, size.height/50);
@@ -57,8 +68,7 @@ class CurvedDottedLinePainter extends CustomPainter {
         size.width * (2 / 3), size.height - (size.height * (1.497 / 2.044)),
         size.width, size.height,
       );
-
-    return path;
+      return path;
   }
 
   @override
