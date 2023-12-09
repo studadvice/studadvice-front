f70e758d3810f90b6176bf2c37a2137075ead50d
amevigbe <yaoisidore.amevigbe.etu@univ-lille.fr>
2023-12-09 21:55:33 +0100

    feat(roadmap) 🚀: Amélioration de l'arc de liaison
    Ajout des couleurs sur les numéros d'étapes 🖋️
    Ajout des étapes avec les liaisons
    
    DEVSTUD-39 💻

--- lib/src/screens/roadmap/roadmap_screen.dart
+++ lib/src/screens/roadmap/roadmap_screen.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:stud_advice/src/models/step/step_item_data.dart';
 import 'package:stud_advice/src/widgets/common/paints/curved_dottedline/curved_dottedline_paint.dart';
 import 'package:stud_advice/src/widgets/common/paints/step_number/step_number_widget.dart';
 import 'package:stud_advice/src/widgets/common/paints/steps_indicator_widget.dart';
 
 class RoadMapScreen extends StatelessWidget {
   static const navigatorId = '/roadmap';
+  final int stepCount;
+  final int currentStep;
+  List<StepItem> steps = [
+    StepItem(number: 1, title: 'Step 1', isCompleted: true),
+    StepItem(number: 2, title: 'Step 2', isCompleted: true),
+    StepItem(number: 3, title: 'Step 3', isCompleted: true),
+    StepItem(number: 4, title: 'Step 4', isCompleted: true),
+    StepItem(number: 5, title: 'Step 5', isCompleted: true)
+  ];
 
+  RoadMapScreen({
+    super.key,
+    required this.stepCount,
+    required this.currentStep,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: StepsIndicator(stepCount: 3),
+    return Scaffold(
+      body: Center(
+        child: Container(
+          // add margin left and right
+          margin: const EdgeInsets.symmetric(horizontal: 30),
+          child: Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: StepsIndicator(
+              stepCount: stepCount,
+              currentStep: currentStep,
+              steps: steps,
+            ),
+          ),
+        )
+      ),
     );
   }
 }
--- lib/src/widgets/common/paints/curved_dottedline/curved_dottedline_paint.dart
+++ lib/src/widgets/common/paints/curved_dottedline/curved_dottedline_paint.dart
@@ -1,4 +1,5 @@
 import 'dart:core';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:path_drawing/path_drawing.dart';
@@ -7,6 +8,7 @@ class CurvedDottedLinePainter extends CustomPainter {
   final bool isLeft;
   final Color color;
   final double strokeWidth;
+
 
   CurvedDottedLinePainter({
     required this.isLeft,
@@ -22,43 +24,43 @@ class CurvedDottedLinePainter extends CustomPainter {
       ..strokeWidth = strokeWidth;
 
     Path path = Path();
+    path = _buildCurvePathIfIsRight(path, size);
 
     if (isLeft) {
-      path = _buildCurvePathIfIsLeft(path, size);
-    } else {
-      path = _buildCurvePathIfIsRight(path, size);
+      path = _buildMirrorCurvePath(path, size);
     }
 
     canvas.drawPath(
         dashPath(path, dashArray: CircularIntervalList([10, 9.5])), paint);
   }
 
-  _buildCurvePathIfIsLeft(Path path, Size size) {
-    // Commencez à partir du côté droit au lieu du côté gauche
-    path.moveTo(size.width, size.height / 50);
+  Path _buildMirrorCurvePath(Path originalPath, Size size) {
+    PathMetric pathMetric = originalPath.computeMetrics().last;
+    Tangent? tangent = pathMetric.getTangentForOffset(pathMetric.length);
+    Offset endPoint = tangent!.position;
 
-    // Ajoutez la courbe de Bézier cubique en inversant les coordonnées X
-    path.cubicTo(
-        size.height + (size.height * (1 / 3)), size.width - (size.width * (0.7 / 2)),
-      size.width - (size.width * (2 / 3)), size.height - (size.height * (1.497 / 2.044)),
-      0, size.height,
-    );
+    Path mirrorPath = Path.from(originalPath);
 
-    return path;
+    final Matrix4 matrix = Matrix4.identity()
+      ..translate(0.0, -endPoint.dy)
+      ..scale(1.0, -1.0)
+      ..translate(0.0, endPoint.dy);
+
+    mirrorPath = mirrorPath.transform(matrix.storage);
+
+    mirrorPath = mirrorPath.shift(Offset(0, 3 * endPoint.dy));
+
+    return mirrorPath;
   }
 
-
   _buildCurvePathIfIsRight(Path path, Size size) {
-      path.moveTo(20, size.height/50);
-
-      // Add the cubic bezier curve
+      path.moveTo(10, size.height/20);
       path.cubicTo(
-        size.width * (0.7 / 2), size.height + (size.height * (1 / 3)),
-        size.width * (2 / 3), size.height - (size.height * (1.497 / 2.044)),
+        size.width * (0.6 / 2), size.height + (size.height * (1 / 3)),
+        size.width * (2 / 3), size.height - (size.height * (2.6 / 2.044)),
         size.width, size.height,
       );
-
-    return path;
+      return path;
   }
 
   @override
