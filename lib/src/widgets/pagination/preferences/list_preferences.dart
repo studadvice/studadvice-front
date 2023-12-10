9ae921a2ed8a348613d2d1b99be0cdad0bdc9abd
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-12-10 20:25:44 +0100

    refactor(clean): remove useless classes

--- lib/src/widgets/pagination/preferences/list_preferences.dart
+++
@@ -1,7 +0,0 @@
-/// Wraps the filtering and sorting options for a list of articles.
-class ListPreferences {
-  ListPreferences({
-    this.filteredPlatformIds,
-  });
-  final List<int>? filteredPlatformIds;
-}
