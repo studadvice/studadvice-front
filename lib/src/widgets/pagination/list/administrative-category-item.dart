aeddc06a9da3b00e933bda8afe9c7d6c25e8b67f
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-12-10 17:42:33 +0100

    style(categories): improve style

--- lib/src/widgets/pagination/list/administrative-category-item.dart
+++ lib/src/widgets/pagination/list/administrative-category-item.dart
@@ -1,49 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 import '../../../models/stud_advice/AdministrativeProcessCategory.dart';
 
 class AdministrativeCategoryItem extends StatelessWidget {
-  const AdministrativeCategoryItem({
-    required this.category,
-    super.key
-  });
+
+  const AdministrativeCategoryItem({required this.category, super.key});
+
   final CategoryContent category;
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
     return Card(
+      color: Colors.orange,
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(30),
+        borderRadius: BorderRadius.circular(16),
       ),
-      child: InkWell(
-        child: Padding(
-          padding: const EdgeInsets.all(50),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Text(
-                      "test",
-                      style: textTheme.titleMedium,
-                    ),
+      child: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  category.name,
+                  style: const TextStyle(color: Colors.white),
+                ),
+                const SizedBox(height: 4),
+                const Text(
+                  "35",
+                  style: TextStyle(color: Colors.white),
+                ),
+                const SizedBox(height: 4),
+                CircularPercentIndicator(
+                  radius: 30,
+                  lineWidth: 8,
+                  animation: true,
+                  animationDuration: 1500,
+                  circularStrokeCap: CircularStrokeCap.round,
+                  percent: 0.7,
+                  progressColor: Colors.white,
+                  center: const Text(
+                    "${70}%",
+                    style: TextStyle(color: Colors.white),
                   ),
-                  const SizedBox(
-                    width: 16,
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 80,
-              ),
-              Text(
-                "test",
-                style: textTheme.bodyMedium,
-              )
-            ],
-          ),
+                )
+              ],
+            ),
+            const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image(
+                  image: AssetImage(
+                      'assets/images/common/raven_image.png'),
+                  height: 110,
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
