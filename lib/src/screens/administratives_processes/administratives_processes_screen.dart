7ae3580daa3d5d3d3fe0911c2f981effdca2e8b6
Churchill Atchedji <churchill.atchedji@limpidius.com>
2023-12-10 12:59:48 +0100

    imp(admin_process): improve admin process screen skeleton

--- lib/src/screens/administratives_processes/administratives_processes_screen.dart
+++ lib/src/screens/administratives_processes/administratives_processes_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:stud_advice/stud_advice.dart';
+import 'package:get/get.dart';
+import 'package:stud_advice/src/common/chore.dart';
 
 class AdministrativesProcessesScreen extends StatelessWidget {
   static const String navigatorId = '/administratives_processes_screen';
@@ -8,19 +9,47 @@ class AdministrativesProcessesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: AppColors.blue,
-      // Bouton de retour à la page précédente AppBar
-      // Icon en haut à gauche
+    return Scaffold(
+      backgroundColor: AppColors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Santé'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Get.back();
+          },
+        ),
+      ),
+
       // Nom de la démarche à coté à droite en mode space between
       // Description de la démarche en mode légende avec trois points de suspension visible à l'expansion de la card ???
       // Bouton d'expansion de la card
       // Barre de progression de la démarche (visible et optionnel via un paramètre)
       // Bouton de mise en favoris de la démarche (coeur ou étoile) -- les démarches favorites peuvent êtres stockées via firebase et fetch au démarrage de l'application
 
-      //
-      body: Center(
-        child: Text('AdministrativesProcessesScreen'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.white,
+              child: const Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Administratives Processes Screen',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
