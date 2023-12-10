725d9739b06741e5b21d2a2e9d0fa90ee95b10bf
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-12-10 17:09:45 +0100

    fix(model): fix administrative process category model depending on the nullable field

--- lib/src/models/stud_advice/AdministrativeProcessCategory.dart
+++ lib/src/models/stud_advice/AdministrativeProcessCategory.dart
@@ -1,85 +1,82 @@
 class AdministrativeProcessCategory {
-  List<CategoryContent>? content;
-  Pageable? pageable;
+  late List<CategoryContent> content;
+  late Pageable pageable;
   late int totalElements;
-  int? totalPages;
+  late int totalPages;
   late bool last;
-  int? size;
-  int? number;
-  Sort? sort;
-  int? numberOfElements;
-  bool? first;
-  bool? empty;
+  late int size;
+  late int number;
+  late Sort sort;
+  late int numberOfElements;
+  late bool first;
+  late bool empty;
 
   AdministrativeProcessCategory(
-      {this.content,
-        this.pageable,
+      {
+        required this.content,
+        required this.pageable,
         required this.totalElements,
-        this.totalPages,
+        required this.totalPages,
         required this.last,
-        this.size,
-        this.number,
-        this.sort,
-        this.numberOfElements,
-        this.first,
-        this.empty});
+        required this.size,
+        required this.number,
+        required this.sort,
+        required this.numberOfElements,
+        required this.first,
+        required this.empty
+      });
 
   AdministrativeProcessCategory.fromJson(Map<String, dynamic> json) {
     if (json['content'] != null) {
       content = <CategoryContent>[];
       json['content'].forEach((v) {
-        content!.add(new CategoryContent.fromJson(v));
+        content.add(CategoryContent.fromJson(v));
       });
     }
-    pageable = json['pageable'] != null
-        ? new Pageable.fromJson(json['pageable'])
-        : null;
+    pageable = (json['pageable'] != null
+        ? Pageable.fromJson(json['pageable'])
+        : null)!;
     totalElements = json['totalElements'];
     totalPages = json['totalPages'];
     last = json['last'];
     size = json['size'];
     number = json['number'];
-    sort = json['sort'] != null ? new Sort.fromJson(json['sort']) : null;
+    sort = (json['sort'] != null ? Sort.fromJson(json['sort']) : null)!;
     numberOfElements = json['numberOfElements'];
     first = json['first'];
     empty = json['empty'];
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    if (this.content != null) {
-      data['content'] = this.content!.map((v) => v.toJson()).toList();
-    }
-    if (this.pageable != null) {
-      data['pageable'] = this.pageable!.toJson();
-    }
-    data['totalElements'] = this.totalElements;
-    data['totalPages'] = this.totalPages;
-    data['last'] = this.last;
-    data['size'] = this.size;
-    data['number'] = this.number;
-    if (this.sort != null) {
-      data['sort'] = this.sort!.toJson();
-    }
-    data['numberOfElements'] = this.numberOfElements;
-    data['first'] = this.first;
-    data['empty'] = this.empty;
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['content'] = content.map((v) => v.toJson()).toList();
+    data['pageable'] = pageable.toJson();
+      data['totalElements'] = totalElements;
+    data['totalPages'] = totalPages;
+    data['last'] = last;
+    data['size'] = size;
+    data['number'] = number;
+    data['sort'] = sort.toJson();
+      data['numberOfElements'] = numberOfElements;
+    data['first'] = first;
+    data['empty'] = empty;
     return data;
   }
 }
 
 class CategoryContent {
-  String? id;
-  String? name;
-  String? description;
-  String? imageId;
+  late String id;
+  late String name;
+  late String description;
+  late String imageId;
   List<AdministrativeProcesses>? administrativeProcesses;
 
   CategoryContent(
-      {this.id,
-        this.name,
-        this.description,
-        this.imageId,
+      {
+        required this.id,
+        required this.name,
+        required this.description,
+        required this.imageId,
         this.administrativeProcesses});
 
   CategoryContent.fromJson(Map<String, dynamic> json) {
@@ -90,30 +87,30 @@ class CategoryContent {
     if (json['administrativeProcesses'] != null) {
       administrativeProcesses = <AdministrativeProcesses>[];
       json['administrativeProcesses'].forEach((v) {
-        administrativeProcesses!.add(new AdministrativeProcesses.fromJson(v));
+        administrativeProcesses!.add(AdministrativeProcesses.fromJson(v));
       });
     }
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['name'] = this.name;
-    data['description'] = this.description;
-    data['imageId'] = this.imageId;
-    if (this.administrativeProcesses != null) {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['description'] = description;
+    data['imageId'] = imageId;
+    if (administrativeProcesses != null) {
       data['administrativeProcesses'] =
-          this.administrativeProcesses!.map((v) => v.toJson()).toList();
+          administrativeProcesses!.map((v) => v.toJson()).toList();
     }
     return data;
   }
 }
 
 class AdministrativeProcesses {
-  String? id;
-  String? name;
-  String? description;
-  String? imageId;
+  late String id;
+  late String name;
+  late String description;
+  late String imageId;
   List<String>? educations;
   int? minAge;
   int? maxAge;
@@ -122,10 +119,11 @@ class AdministrativeProcesses {
   List<Steps>? steps;
 
   AdministrativeProcesses(
-      {this.id,
-        this.name,
-        this.description,
-        this.imageId,
+      {
+        required this.id,
+        required this.name,
+        required this.description,
+        required this.imageId,
         this.educations,
         this.minAge,
         this.maxAge,
@@ -146,44 +144,44 @@ class AdministrativeProcesses {
     if (json['steps'] != null) {
       steps = <Steps>[];
       json['steps'].forEach((v) {
-        steps!.add(new Steps.fromJson(v));
+        steps!.add(Steps.fromJson(v));
       });
     }
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['name'] = this.name;
-    data['description'] = this.description;
-    data['imageId'] = this.imageId;
-    data['educations'] = this.educations;
-    data['minAge'] = this.minAge;
-    data['maxAge'] = this.maxAge;
-    data['nationalities'] = this.nationalities;
-    data['universities'] = this.universities;
-    if (this.steps != null) {
-      data['steps'] = this.steps!.map((v) => v.toJson()).toList();
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['description'] = description;
+    data['imageId'] = imageId;
+    data['educations'] = educations;
+    data['minAge'] = minAge;
+    data['maxAge'] = maxAge;
+    data['nationalities'] = nationalities;
+    data['universities'] = universities;
+    if (steps != null) {
+      data['steps'] = steps!.map((v) => v.toJson()).toList();
     }
     return data;
   }
 }
 
 class Steps {
-  int? stepNumber;
-  String? name;
-  String? description;
-  Null? imageId;
+  late int stepNumber;
+  late String name;
+  late String description;
+  String? imageId;
   List<RequiredDocuments>? requiredDocuments;
-  Null? resources;
 
   Steps(
-      {this.stepNumber,
-        this.name,
-        this.description,
+      {
+        required this.stepNumber,
+        required this.name,
+        required this.description,
         this.imageId,
-        this.requiredDocuments,
-        this.resources});
+        this.requiredDocuments
+      });
 
   Steps.fromJson(Map<String, dynamic> json) {
     stepNumber = json['stepNumber'];
@@ -193,36 +191,39 @@ class Steps {
     if (json['requiredDocuments'] != null) {
       requiredDocuments = <RequiredDocuments>[];
       json['requiredDocuments'].forEach((v) {
-        requiredDocuments!.add(new RequiredDocuments.fromJson(v));
+        requiredDocuments!.add(RequiredDocuments.fromJson(v));
       });
     }
-    resources = json['resources'];
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['stepNumber'] = this.stepNumber;
-    data['name'] = this.name;
-    data['description'] = this.description;
-    data['imageId'] = this.imageId;
-    if (this.requiredDocuments != null) {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['stepNumber'] = stepNumber;
+    data['name'] = name;
+    data['description'] = description;
+    data['imageId'] = imageId;
+    if (requiredDocuments != null) {
       data['requiredDocuments'] =
-          this.requiredDocuments!.map((v) => v.toJson()).toList();
+          requiredDocuments!.map((v) => v.toJson()).toList();
     }
-    data['resources'] = this.resources;
     return data;
   }
 }
 
 class RequiredDocuments {
-  String? id;
-  String? name;
-  String? description;
-  Null? imageId;
-  Null? url;
+  late String id;
+  late String name;
+  late String description;
+  String? imageId;
+  String? url;
 
   RequiredDocuments(
-      {this.id, this.name, this.description, this.imageId, this.url});
+      {
+        required this.id,
+        required this.name,
+        required this.description,
+        this.imageId,
+        this.url});
 
   RequiredDocuments.fromJson(Map<String, dynamic> json) {
     id = json['id'];
@@ -233,61 +234,66 @@ class RequiredDocuments {
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['name'] = this.name;
-    data['description'] = this.description;
-    data['imageId'] = this.imageId;
-    data['url'] = this.url;
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['description'] = description;
+    data['imageId'] = imageId;
+    data['url'] = url;
     return data;
   }
 }
 
 class Pageable {
-  int? pageNumber;
-  int? pageSize;
-  Sort? sort;
-  int? offset;
-  bool? paged;
-  bool? unpaged;
+  late int pageNumber;
+  late int pageSize;
+  late Sort sort;
+  late int offset;
+  late bool paged;
+  late bool unpaged;
 
   Pageable(
-      {this.pageNumber,
-        this.pageSize,
-        this.sort,
-        this.offset,
-        this.paged,
-        this.unpaged});
+      {
+        required this.pageNumber,
+        required this.pageSize,
+        required this.sort,
+        required this.offset,
+        required this.paged,
+        required this.unpaged
+      });
 
   Pageable.fromJson(Map<String, dynamic> json) {
     pageNumber = json['pageNumber'];
     pageSize = json['pageSize'];
-    sort = json['sort'] != null ? new Sort.fromJson(json['sort']) : null;
+    sort = (json['sort'] != null ? Sort.fromJson(json['sort']) : null)!;
     offset = json['offset'];
     paged = json['paged'];
     unpaged = json['unpaged'];
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['pageNumber'] = this.pageNumber;
-    data['pageSize'] = this.pageSize;
-    if (this.sort != null) {
-      data['sort'] = this.sort!.toJson();
-    }
-    data['offset'] = this.offset;
-    data['paged'] = this.paged;
-    data['unpaged'] = this.unpaged;
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['pageNumber'] = pageNumber;
+    data['pageSize'] = pageSize;
+    data['sort'] = sort.toJson();
+    data['offset'] = offset;
+    data['paged'] = paged;
+    data['unpaged'] = unpaged;
     return data;
   }
 }
 
 class Sort {
-  bool? empty;
-  bool? sorted;
-  bool? unsorted;
+  late bool empty;
+  late bool sorted;
+  late bool unsorted;
 
-  Sort({this.empty, this.sorted, this.unsorted});
+  Sort(
+      {
+        required this.empty,
+        required this.sorted,
+        required this.unsorted
+      });
 
   Sort.fromJson(Map<String, dynamic> json) {
     empty = json['empty'];
@@ -296,10 +302,10 @@ class Sort {
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['empty'] = this.empty;
-    data['sorted'] = this.sorted;
-    data['unsorted'] = this.unsorted;
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['empty'] = empty;
+    data['sorted'] = sorted;
+    data['unsorted'] = unsorted;
     return data;
   }
 }
