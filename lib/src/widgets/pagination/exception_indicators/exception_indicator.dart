8ac6bc739e4bdbc8064fe8b21142ae7e2781f5bb
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-12-10 13:14:42 +0100

    feat(pagination-exceptions): add infinite paginations differents exceptions

---
+++ lib/src/widgets/pagination/exception_indicators/exception_indicator.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class ExceptionIndicator extends StatelessWidget {
+  const ExceptionIndicator({
+    required this.title,
+    required this.assetName,
+    this.message,
+    this.onTryAgain,
+    super.key,
+  });
+  final String title;
+  final String? message;
+  final String assetName;
+  final VoidCallback? onTryAgain;
+
+  @override
+  Widget build(BuildContext context) => Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+          child: Column(
+            children: [
+              Image.asset(
+                assetName,
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              Text(
+                title,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              if (message != null)
+                const SizedBox(
+                  height: 16,
+                ),
+              if (message != null)
+                Text(
+                  message!,
+                  textAlign: TextAlign.center,
+                ),
+              if (onTryAgain != null) const Spacer(),
+              if (onTryAgain != null)
+                SizedBox(
+                  height: 50,
+                  width: double.infinity,
+                  child: ElevatedButton.icon(
+                    onPressed: onTryAgain,
+                    icon: const Icon(
+                      Icons.refresh,
+                      color: Colors.white,
+                    ),
+                    label: const Text(
+                      'Try Again',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      );
+}
