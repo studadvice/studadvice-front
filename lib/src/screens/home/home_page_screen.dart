3c9dd496d745d2783871974bc303ca08558f5384
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-12-10 20:27:09 +0100

    feat(parameters): integer parameters in the home page

--- lib/src/screens/home/home_page_screen.dart
+++ lib/src/screens/home/home_page_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:stud_advice/src/widgets/pagination/categories/categories_list_screen.dart';
+import 'package:stud_advice/src/widgets/search/CategoriesSearch.dart';
 import 'package:stud_advice/stud_advice.dart';
-
-import '../../widgets/pagination/list/administrative-category-list-screen.dart';
 
 class HomePageScreen extends StatelessWidget {
   static const navigatorId = '/home_page_screen';
@@ -11,8 +11,35 @@ class HomePageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        iconTheme: const IconThemeData(color: Colors.grey, size: 28),
+        actions: [
+          IconButton(
+            onPressed: () {
+              showSearch(context: context, delegate: CategoriesSearch());
+            },
+            icon: const Icon(
+              Icons.search,
+              color: Colors.grey,
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Get.toNamed(SettingsScreen.navigatorId);
+            },
+            child: Container(
+              margin: const EdgeInsets.only(top: 5, right: 16, bottom: 5),
+              child: const CircleAvatar(
+                backgroundImage: AssetImage('assets/images/common/raven_image.png'),
+              ),
+            ),
+          )
+        ],
+      ),
       body: Center(
-        child: AdministrativeCategoryListScreen(),
+        child: CategoriesListScreen(),
       ),
     );
     // },
