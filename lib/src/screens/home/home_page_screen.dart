3a373dc4f0bb302cda1d4f02d32b9de7b444e2c7
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-12-10 09:03:18 +0100

    feat(testing-infinite-pagination): add exceptions

--- lib/src/screens/home/home_page_screen.dart
+++ lib/src/screens/home/home_page_screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:stud_advice/src/widgets/pagination/list/categories_list_screen.dart';
 import 'package:stud_advice/stud_advice.dart';
+
+import '../../repositories/stud_advice/stud_advice.dart';
 
 class HomePageScreen extends StatelessWidget {
   static const navigatorId = '/home_page_screen';
 
   @override
   Widget build(BuildContext context) {
+    Get.put(StudAdviceRepository());
     // return GetBuilder(
       // builder: (controller) {
         return Scaffold(
@@ -20,31 +24,7 @@ class HomePageScreen extends StatelessWidget {
             ),
           ),
           body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Welcome to the Home Page!',
-                  style: TextStyle(
-                    fontSize: AppFontSizes.extraLarge,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  child: CustomButton(
-                    text: "Settings",
-                    textColor: AppColors.white,
-                    backgroundColor: AppColors.dangerColor,
-                    onPressed: () {
-                      Get.toNamed(SettingsScreen.navigatorId);
-                    },
-                  ),
-                ),
-              ],
-            ),
+            child: CategoriesListScreen(),
           ),
         );
       // },
---
+++ lib/src/widgets/pagination/exception_indicators/generic_error_indicator.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+
+import 'exception_indicator.dart';
+
+class GenericErrorIndicator extends StatelessWidget {
+  const GenericErrorIndicator({
+    super.key,
+    this.onTryAgain,
+  });
+
+  final VoidCallback? onTryAgain;
+
+  @override
+  Widget build(BuildContext context) => ExceptionIndicator(
+        title: 'Something went wrong',
+        message: 'The application has encountered an unknown error.\n'
+            'Please try again later.',
+        assetName: 'assets/confused-face.png',
+        onTryAgain: onTryAgain,
+      );
+}
