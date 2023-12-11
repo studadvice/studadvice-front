922e0787d4ea1a31ca1a7f3ca0cfe2b4816f40e7
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-12-11 14:40:44 +0100

    feat(search): add category search class

---
+++ lib/src/widgets/search/CategoriesSearch.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:stud_advice/src/widgets/pagination/search_categories/search_categories_list_screen.dart';
+
+class CategoriesSearch extends SearchDelegate<String> {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context,"");
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: SearchCategoriesListScreen(searchQuery: query),
+        )
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return Container(
+    );
+  }
+}
