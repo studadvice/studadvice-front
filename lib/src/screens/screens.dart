783837cfd6b27a54743e2e3be3bdb9c74d5dfeb7
Churchill Atchedji <churchill.atchedji@limpidius.com>
2023-12-12 16:29:50 +0100

    feat(admin_process): add administrative process model

--- lib/src/screens/screens.dart
+++ lib/src/screens/screens.dart
@@ -1,6 +1,6 @@
 library screens;
 
-export 'administratives_processes/administratives_processes_screen.dart';
+export 'administrative_processes/administrative_processes_screen.dart';
 export 'forgot_password/forgot_password_screen.dart';
 export 'home/home_page_screen.dart';
 export 'init_time/init_time_screen.dart';
