18a13e38ba7d41dd7e5d8319480a95b79b311e9e
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-12-16 12:15:39 +0100

    feat(token): add interceptor to automatically add token before request

--- lib/src/controllers/categories/category_controller.dart
+++ lib/src/controllers/categories/category_controller.dart
@@ -3,29 +3,13 @@ import 'package:get/get.dart';
 import 'package:stud_advice/src/models/stud_advice/category.dart';
 
 class CategoryController extends GetxController {
-  final Dio _dio = Get.find<Dio>();
-  final String baseUrl = 'http://localhost:8080';
-
-  String _generateToken() {
-    return "eyJhbGciOiJSUzI1NiIsImtpZCI6ImJlNzgyM2VmMDFiZDRkMmI5NjI3NDE2NThkMjA4MDdlZmVlNmRlNWMiLCJ0eXAiOiJKV1QifQ.eyJjdXN0b21fY2xhaW1zIjpbIkFETUlOIl0sImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9zdHVkYWR2aWNlLWF1dGgiLCJhdWQiOiJzdHVkYWR2aWNlLWF1dGgiLCJhdXRoX3RpbWUiOjE3MDIzMzI1NjYsInVzZXJfaWQiOiJIUmdScml1ZWY5V3V0cGxsYU0xSGZydlNBcUUyIiwic3ViIjoiSFJnUnJpdWVmOVd1dHBsbGFNMUhmcnZTQXFFMiIsImlhdCI6MTcwMjMzMjU2NiwiZXhwIjoxNzAyMzM2MTY2LCJlbWFpbCI6Im5hZGluZS56aW5lLjdAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbIm5hZGluZS56aW5lLjdAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.EYq3VqqyVHAY5gZ1IQpldXKah3EOweaZSRiw3TslHF9cM8OagSHA_C7iwZLMDL3CcMOIx9ZaeB4dpFbVEup6Yef-csY5jCYfWM0M4-z8e4-NPekp499UKEofGeCASvYZ2hucxU0K4igIMAk1e7ZloNHbXG3P-FpfjLD65R-NmJE0CxR9xGGS3QwyIBDb3PxMknw6hs-pvYiopaN9QSfQ1xhx-hHwimXVK94we6G6a6P1uEFoftLFXxULhw3pzZz9IuDBJIknhnFhybrD3n0DPfJQs0cfSQRLSirZjpTWaBasH0ldma6xZhYudLh38440oWxBey4oLv11NcNTcvdJOg";
-  }
-
-  Options _getOptions() {
-    return Options(
-      headers: {
-        'content-Type': 'application/json',
-        'accept': 'application/json',
-        'Authorization': 'Bearer ${_generateToken()}',
-      },
-    );
-  }
+  final Dio _dio = Get.find();
 
   Future<Category> _getCategories(String path, Map<String, dynamic> queryParameters) async {
     try {
       final response = await _dio.get(
-        '$baseUrl/$path',
+        path,
         queryParameters: queryParameters,
-        options: _getOptions(),
       );
 
       if (response.statusCode == 200) {
@@ -34,6 +18,7 @@ class CategoryController extends GetxController {
         throw Exception('Failed to load categories');
       }
     } catch (error) {
+      print(error);
       rethrow;
     }
   }
@@ -41,9 +26,8 @@ class CategoryController extends GetxController {
   Future<Category> _getCategoriesBySearch(String path, Map<String, dynamic> queryParameters) async {
     try {
       final response = await _dio.get(
-        '$baseUrl/$path',
+        path,
         queryParameters: queryParameters,
-        options: _getOptions(),
       );
 
       if (response.statusCode == 200) {
@@ -58,7 +42,7 @@ class CategoryController extends GetxController {
 
   Future<Category> getCategories({required int number, required int size}) async {
     final queryParameters = {'page': number, 'size': size};
-    return _getCategories('categories', queryParameters);
+    return _getCategories('/categories', queryParameters);
   }
 
   Future<Category> getCategoriesBySearch({
@@ -67,6 +51,6 @@ class CategoryController extends GetxController {
     required String query,
   }) async {
     final queryParameters = {'page': number, 'size': size, 'searchText': query};
-    return _getCategoriesBySearch('administrative-process/search', queryParameters);
+    return _getCategoriesBySearch('/administrative-process/search', queryParameters);
   }
 }
