7e48d49545ba3690c5fe051e4965e88e98ab718f
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-12-16 12:15:39 +0100

    feat(token): add interceptor to automatically add token before request

--- lib/src/bindings/app/app_binding.dart
+++ lib/src/bindings/app/app_binding.dart
@@ -2,10 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
-import 'package:stud_advice/src/controllers/categories/category_controller.dart';
 import 'package:stud_advice/src/controllers/controllers.dart';
-
-import '../../controllers/file/file_controller.dart';
+import 'package:stud_advice/src/middlewares/common/auth_interceptor.dart';
 
 class AppDependenciesBinding extends Bindings {
   static final FirebaseAuth firebaseAuthInstance = FirebaseAuth.instance;
@@ -14,19 +12,23 @@ class AppDependenciesBinding extends Bindings {
 
   @override
   void dependencies() {
-    // Register the chore services.
-    Get.put(Dio(), permanent: true);
+    Dio dio = Dio(
+        BaseOptions(
+          baseUrl: "http://localhost:8080",
+          connectTimeout: const Duration(seconds: 5),
+          receiveTimeout: const Duration(seconds: 5),
+        ));
+    dio.interceptors.add(AuthInterceptor());
+
+    Get.put(dio, permanent: true);
+
+    Get.put(AuthenticationController(), permanent: true);
+
     Get.put(UserStorageController(), permanent: true);
-    Get.put(CategoryController(), permanent: true);
     Get.put(FileController(), permanent: true);
 
     // Language and theme controllers.
     Get.put(I18n(), permanent: true);
     Get.put(ThemeController(), permanent: true);
-
-    // Register the firebase services.
-    Get.put(AuthenticationController(), permanent: true);
-
-    Get.put(CategoryController(), permanent: true);
   }
 }
