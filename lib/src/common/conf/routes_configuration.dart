15bec811f4fddba83d7246a236b518da51ee75b8
isidoreamevigbe <amevigbe41@gmail.com>
2023-12-17 23:12:40 +0100

    feat(roadmap): ajout d'un controlleur pour obtenir le détail sur les étapes
    DEVSTUD-39

--- lib/src/common/conf/routes_configuration.dart
+++ lib/src/common/conf/routes_configuration.dart
@@ -1,55 +1,56 @@
 import 'package:get/get.dart';
+import 'package:stud_advice/src/screens/roadmap/roadmap_screen.dart';
 import 'package:stud_advice/stud_advice.dart';
 
 class RoutesConfiguration {
-  // TODO: update initialRoute to welcome screen
-  static const String initialRoute = AdministrativesProcessesScreen.navigatorId;
-
+  static const String initialRoute = RoadMapScreen.navigatorId;
   List<GetPage> configureRoutes() {
     return [
       GetPage(
-        name: WelcomeScreen.navigatorId,
-        page: () => const WelcomeScreen(),
-        // middlewares: [I18nMiddleware()],
-      ),
-      GetPage(
         name: InitTimeScreen.navigatorId,
         page: () => InitTimeScreen(),
-        // middlewares: [I18nMiddleware()],
+        middlewares: [I18nMiddleware()],
+      ),
+      GetPage(
+        name: WelcomeScreen.navigatorId,
+        page: () => const WelcomeScreen(),
+        middlewares: [I18nMiddleware()],
       ),
       GetPage(
         name: HomePageScreen.navigatorId,
         page: () => HomePageScreen(),
-        binding: CategoryBinding(),
         middlewares: [I18nMiddleware()],
       ),
       GetPage(
         name: LoginScreen.navigatorId,
         page: () => LoginScreen(),
+        middlewares: [I18nMiddleware()],
         binding: LoginBinding(),
       ),
       GetPage(
         name: RegisterOptionsScreen.navigatorId,
         page: () => RegisterOptionsScreen(),
-        binding: RegisterOptionsBinding(),
+        middlewares: [I18nMiddleware()],
       ),
       GetPage(
         name: RegisterWithEmailScreen.navigatorId,
         page: () => RegisterWithEmailScreen(),
-        binding: RegisterWithEmailBinding(),
+        middlewares: [I18nMiddleware()],
+      ),
+      GetPage(
+        name: LegalTermsScreen.navigatorId,
+        page: () => const LegalTermsScreen(),
+        middlewares: [I18nMiddleware()],
       ),
       GetPage(
         name: RegisterUserInformationScreen.navigatorId,
         page: () => const RegisterUserInformationScreen(),
-        binding: RegisterUserInformationBinding(),
-      ),
-      GetPage(
-        name: LegalTermsScreen.navigatorId,
-        page: () => LegalTermsScreen(),
+        middlewares: [I18nMiddleware()],
       ),
       GetPage(
         name: SettingsScreen.navigatorId,
         page: () => SettingsScreen(),
+        middlewares: [I18nMiddleware()],
         binding: SettingsBinding(),
       ),
       GetPage(
@@ -57,22 +58,6 @@ class RoutesConfiguration {
         page: () => RoadMapScreen(
           currentStep: 1,
         ),
-        middlewares: [I18nMiddleware()],
-      ),
-      GetPage(
-        name: ForgotPasswordScreen.navigatorId,
-        page: () => ForgotPasswordScreen(),
-        binding: ForgotPasswordBinding(),
-      ),
-      GetPage(
-        name: RoadMapScreen.navigatorId,
-        page: () => RoadMapScreen(currentStep: 0,),
-        middlewares: [I18nMiddleware()],
-      ),
-      GetPage(
-        name: AdministrativesProcessesScreen.navigatorId,
-        page: () => AdministrativesProcessesScreen(),
-        binding: AdministrativeProcessBinding(),
         middlewares: [I18nMiddleware()],
       )
     ];
--- lib/src/screens/roadmap/roadmap_screen.dart
+++ lib/src/screens/roadmap/roadmap_screen.dart
@@ -1,45 +1,136 @@
 import 'package:flutter/material.dart';
-import 'package:stud_advice/src/models/step/step_item_data.dart';
-import 'package:stud_advice/src/widgets/common/paints/curved_dottedline/curved_dottedline_paint.dart';
-import 'package:stud_advice/src/widgets/common/paints/step_number/step_number_widget.dart';
-import 'package:stud_advice/src/widgets/common/paints/steps_indicator_widget.dart';
+import 'package:get/get.dart';
+import 'package:stud_advice/stud_advice.dart';
 
 class RoadMapScreen extends StatelessWidget {
   static const navigatorId = '/roadmap';
-  final int stepCount;
   final int currentStep;
   List<StepItem> steps = [
-    StepItem(number: 1, title: 'Step 1', isCompleted: true, color: Colors.green),
-    StepItem(number: 2, title: 'Step 2', isCompleted: true, color: Colors.lightBlue),
-    StepItem(number: 3, title: 'Step 3', isCompleted: true, color: Colors.blue),
-    StepItem(number: 4, title: 'Step 4', isCompleted: true, color: Colors.orange),
-    StepItem(number: 5, title: 'Step 4', isCompleted: true, color: Colors.green),
-    StepItem(number: 6, title: 'Step 4', isCompleted: true, color: Colors.lightBlue),
-    StepItem(number: 7, title: 'Step 4', isCompleted: true, color: Colors.orange),
+    StepItem(
+        number: 1,
+        title: 'Titre de séjour',
+        isCompleted: true,
+        color: Colors.green,
+        description: "Pour obtenir un titre de séjour, vous devez vous rendre à la préfecture de votre lieu de résidence."
+    ),
+    StepItem(
+        number: 2,
+        title: 'Ouverture d’un compte bancaire',
+        isCompleted: true,
+        color: Colors.lightBlue,
+        description: "Pour ouvrir un compte bancaire, vous devez vous rendre à la banque de votre choix."
+    ),
+    StepItem(
+        number: 3,
+        title: 'Sécurité sociale',
+        isCompleted: true,
+        color: Colors.blue,
+        description: "Pour obtenir un numéro de sécurité sociale, vous devez vous rendre à la CPAM de votre lieu de résidence."
+    ),
+    StepItem(
+        number: 4,
+        title: 'CAF',
+        isCompleted: true,
+        color: Colors.orange,
+        description: "Pour beneficier des aides de la CAF, vous devez vous rendre à la CAF de votre lieu de résidence."
+    ),
+    StepItem(
+        number: 5,
+        title: 'Assurance logement',
+        isCompleted: true,
+        color: Colors.green,
+        description: "Pour obtenir une assurance logement, vous devez vous rendre à l’assurance de votre choix."
+    ),
+    StepItem(
+        number: 6,
+        title: 'Inscription à la fac',
+        isCompleted: true,
+        color: Colors.lightBlue,
+        description: "Pour vous inscrire à la fac, vous devez vous rendre à la fac de votre choix."
+    ),
+    StepItem(
+        number: 7,
+        title: 'Inscription à la bibliothèque',
+        isCompleted: true,
+        color: Colors.orange,
+        description: "Pour vous inscrire à la bibliothèque, vous devez vous rendre à la bibliothèque de votre choix."
+    ),
   ];
+  final StepController stepController = Get.put(StepController());
 
   RoadMapScreen({
     super.key,
-    required this.stepCount,
     required this.currentStep,
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          // add margin left and right
-          margin: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: StepsIndicator(
-              stepCount: stepCount,
-              currentStep: currentStep,
-              steps: steps,
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              flex: 1,
+              child:  Container(
+                margin: const EdgeInsets.symmetric(horizontal: 1.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: StepsIndicator(
+                    currentStep: stepController.currentStep.value,
+                    steps: steps,
+                  ),
+                ),
+              )
             ),
+          const Divider(
+            thickness: 0.3,
+            indent: 5,
+            endIndent: 5,
           ),
-        )
+          Expanded(
+            flex: 1,
+            child: Obx(() {
+              StepItem currentStepItem = steps[stepController.currentStep.value];
+              return Container(
+                padding: const EdgeInsets.all(5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      currentStepItem.title,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    const SizedBox(height: 7),
+                    Text(
+                      currentStepItem.description!,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.black54,
+                      ),
+                    ),
+                    const SizedBox(height: 80),
+                    Center(
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: CustomButton(
+                          text: "Next",
+                          textColor: AppColors.white,
+                          backgroundColor: AppColors.primaryColorAccent,
+                          onPressed: () {
+                            // stepController.changeStep(stepController.currentStep.value + 1);
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }),
+          ),
+        ],
       ),
     );
   }
