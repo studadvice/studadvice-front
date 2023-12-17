ab447b24d070aafe5c85baa68cf3bb846e109aa7
amevigbe <yaoisidore.amevigbe.etu@univ-lille.fr>
2023-12-17 18:49:35 +0100

    feat(roadmap) 🚀: Modification de l'évènement
    Ajout d'un attribut pour activer le bouton 🖋️
    
    DEVSTUD-39 💻

--- lib/src/screens/roadmap/roadmap_screen.dart
+++ lib/src/screens/roadmap/roadmap_screen.dart
@@ -9,11 +9,13 @@ class RoadMapScreen extends StatelessWidget {
   final int stepCount;
   final int currentStep;
   List<StepItem> steps = [
-    StepItem(number: 1, title: 'Step 1', isCompleted: true),
-    StepItem(number: 2, title: 'Step 2', isCompleted: true),
-    StepItem(number: 3, title: 'Step 3', isCompleted: true),
-    StepItem(number: 4, title: 'Step 4', isCompleted: true),
-    StepItem(number: 5, title: 'Step 5', isCompleted: true)
+    StepItem(number: 1, title: 'Step 1', isCompleted: true, color: Colors.green),
+    StepItem(number: 2, title: 'Step 2', isCompleted: true, color: Colors.lightBlue),
+    StepItem(number: 3, title: 'Step 3', isCompleted: true, color: Colors.blue),
+    StepItem(number: 4, title: 'Step 4', isCompleted: true, color: Colors.orange),
+    StepItem(number: 5, title: 'Step 4', isCompleted: true, color: Colors.green),
+    StepItem(number: 6, title: 'Step 4', isCompleted: true, color: Colors.lightBlue),
+    StepItem(number: 7, title: 'Step 4', isCompleted: true, color: Colors.orange),
   ];
 
   RoadMapScreen({
@@ -28,9 +30,9 @@ class RoadMapScreen extends StatelessWidget {
       body: Center(
         child: Container(
           // add margin left and right
-          margin: const EdgeInsets.symmetric(horizontal: 30),
+          margin: const EdgeInsets.symmetric(horizontal: 20.0),
           child: Padding(
-            padding: const EdgeInsets.all(18.0),
+            padding: const EdgeInsets.all(25.0),
             child: StepsIndicator(
               stepCount: stepCount,
               currentStep: currentStep,
--- lib/src/widgets/common/paints/steps_indicator_widget.dart
+++ lib/src/widgets/common/paints/steps_indicator_widget.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:stud_advice/src/models/step/step_item_data.dart';
-import 'package:stud_advice/src/widgets/common/paints/curved_dottedline/curved_dottedline_paint.dart';
-import 'package:stud_advice/src/widgets/common/paints/step_number/step_number_widget.dart';
+import 'package:stud_advice/stud_advice.dart';
 
 class StepsIndicator extends StatelessWidget {
   final int stepCount;
   final int currentStep;
   final List<StepItem> steps;
+  static double DIAMETER = 100.0;
 
 
   const StepsIndicator({
@@ -29,7 +28,7 @@ class StepsIndicator extends StatelessWidget {
           return LayoutBuilder(
             builder: (BuildContext context, BoxConstraints constraints) {
               double screenWidth = constraints.maxWidth;
-              double stepWidgetDiameter = 100;
+              double stepWidgetDiameter = DIAMETER;
               double curveWidth = screenWidth - stepWidgetDiameter;
 
               return Column(
@@ -40,13 +39,17 @@ class StepsIndicator extends StatelessWidget {
                       StepNumberWidget(
                         stepNumber: stepNumber,
                         diameter: stepWidgetDiameter,
-                        color: step.backgroundColor,
+                        color: step.color,
+                        isEnabled: step.isCompleted,
+                        onPressed: () {
+                          print('Step $stepNumber pressed');
+                        },
                       ),
                     ],
                   ),
-                  if (index < stepCount - 1)
+                  if (index < steps.length - 1)
                     CustomPaint(
-                      size: Size(curveWidth, curveWidth/2),
+                      size: Size(curveWidth, curveWidth/2.6),
                       painter: CurvedDottedLinePainter(
                         color: Colors.grey,
                         strokeWidth: 2.0,
