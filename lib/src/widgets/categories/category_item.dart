50aaa50c522d6a838fd6a89e5bb7433ab55e6218
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-12-18 01:37:26 +0100

    style(categories): change category card style

--- lib/src/widgets/categories/category_item.dart
+++ lib/src/widgets/categories/category_item.dart
@@ -12,79 +12,63 @@ class CategoryItem extends StatelessWidget {
   final CategoryContent category;
   final RxBool _isLongPress = false.obs;
 
-
   @override
   Widget build(BuildContext context) {
     Color cardColor = Color(int.parse(category.color, radix: 16) + 0xFF000000);
 
     return GestureDetector(
       onLongPress: () {
-        _isLongPress.value = true;
-      },
+          _isLongPress.value = true;
+        },
       onTap: () {
-        if (_isLongPress.value) {
-          _isLongPress.value = false;
-        } else {
-          Get.toNamed(
-            AdministrativesProcessesScreen.navigatorId,
-            arguments: {
-                'categoryId':
-                category.id,
-                'categoryColor':
-                category.color
-            },
-          );
-        }
-      },
-      child:
-        Card(
-            color: cardColor,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(15),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                    Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text(
-                        category.name,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                      ),
-                      const SizedBox(height: 4),
-                      Text(
-                        '${category.administrativeProcesses!.length} démarches ',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                        ),
-                      )
-                      ]
-                    )
-                    ],
-                  ),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.end, // Align to the right
-                    children: [
-                      Image(
-                        image: AssetImage('assets/images/common/raven_image.png'),
-                        height: 100,
-                      ),
-                    ],
-                  ),
-                ],
+          if (_isLongPress.value) {
+            _isLongPress.value = false;
+          } else {
+            Get.toNamed(
+              AdministrativesProcessesScreen.navigatorId,
+              arguments: {
+                  'categoryId':
+                  category.id,
+                  'categoryColor':
+                  category.color
+              },
+            );
+          }
+        },
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(.1),
+              blurRadius: 4.0,
+              spreadRadius: .05,
+            ), //BoxShadow
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: Image.asset(
+                'assets/images/common/raven_image.png',
+                height: 120,
               ),
             ),
-          ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(category.name),
+            Text(
+              "${category.administrativeProcesses!.length} démarches",
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
