bc661b7b233d43774db4788765f15e7643080a08
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-12-22 22:51:37 +0100

    feat(bottom_bar): add simple bottom navigation bar, will improve it later

---
+++ lib/src/screens/deals/deals_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DealsScreen extends StatelessWidget {
+  static const navigatorId = '/deals_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text(
+        "Deals"
+      ),
+    );
+  }
+}
