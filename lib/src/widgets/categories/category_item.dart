88d96a1ef72ab805a3eee1c372b0dc44f25bf540
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-12-23 17:22:01 +0100

    style(categories): starting to improve categories design

--- lib/src/widgets/categories/category_item.dart
+++ lib/src/widgets/categories/category_item.dart
@@ -17,7 +17,6 @@ class CategoryItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Color cardColor = Color(int.parse(category.color, radix: 16) + 0xFF000000);
-
     return GestureDetector(
       onLongPress: () {
         _isLongPress.value = true;
@@ -38,7 +37,7 @@ class CategoryItem extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.all(10),
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: cardColor,
           borderRadius: BorderRadius.circular(20),
           boxShadow: [
             BoxShadow(
@@ -49,36 +48,66 @@ class CategoryItem extends StatelessWidget {
           ],
         ),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            FutureBuilder<String?>(
-              future: fileController.downloadFile(category.imageId),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done &&
-                    snapshot.data != null) {
-                  return Align(
-                    alignment: Alignment.center,
-                    child: Image.file(
-                      File(snapshot.data!),
-                      height: 120,
-                    ),
-                  );
-                } else if (snapshot.hasError) {
-                  return const Text('Erreur de chargement de l\'image');
-                } else {
-                  return const CircularProgressIndicator();
-                }
-              },
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(category.name),
-            Text(
-              "${category.administrativeProcesses!.length} démarches",
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-          ],
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  category.name,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child:Text(
+                    "${category.administrativeProcesses!.length} démarches",
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      )
+                  )
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: RotationTransition(
+                  turns: const AlwaysStoppedAnimation(20 / 360),
+                  child: FutureBuilder<String?>(
+                    future: fileController.downloadFile(category.imageId),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done &&
+                          snapshot.data != null) {
+                        return Container(
+                          width: 60,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: FileImage(File(snapshot.data!)),
+                              fit: BoxFit.cover,
+                            ),
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(4),
+                              bottomLeft: Radius.circular(4),
+                            ),
+                          ),
+                        );
+                      } else if (snapshot.hasError) {
+                        return const Text('Erreur de chargement de l\'image');
+                      } else {
+                        return const CircularProgressIndicator();
+                      }
+                    },
+                  ),
+                ),
+              )
+            ]
         ),
       ),
     );
--- lib/src/widgets/common/search/custom_search_bar.dart
+++ lib/src/widgets/common/search/custom_search_bar.dart
@@ -5,36 +5,31 @@ import 'package:stud_advice/stud_advice.dart';
 
 class CustomSearchBar extends StatelessWidget {
   final CustomSearchController controller;
-  //final stt.SpeechToText _speech = stt.SpeechToText();
+  final stt.SpeechToText _speech = stt.SpeechToText();
 
   CustomSearchBar({
     super.key,
     required this.controller,
   });
 
-  // void _startListening() async {
-  //   bool available = await _speech.initialize(
-  //     onStatus: (val) => debugPrint('onStatus: $val'),
-  //     onError: (val) => debugPrint('onError: $val'),
-  //   );
-  //
-  //   if (available) {
-  //     _speech.listen(
-  //       onResult: (val) {
-  //         if (controller is SearchCategoryController) {
-  //           //   controller?.textEditingController ?
-  //           //       controller?.textEditingController
-  //           // .text =
-  //           //       val.recognizedWords : '';
-  //           //   (controller as CategoryController).update();
-  //         }
-  //         _speech.stop();
-  //       },
-  //     );
-  //   } else {
-  //     debugPrint("The user has denied the use of speech recognition.");
-  //   }
-  // }
+  void _startListening() async {
+    bool available = await _speech.initialize(
+      onStatus: (val) => debugPrint('onStatus: $val'),
+      onError: (val) => debugPrint('onError: $val'),
+    );
+
+    if (available) {
+      _speech.listen(
+        onResult: (val) {
+          controller.textEditingController.text = val.recognizedWords;
+          controller.update();
+          _speech.stop();
+        },
+      );
+    } else {
+      debugPrint("The user has denied the use of speech recognition.");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,7 +50,7 @@ class CustomSearchBar extends StatelessWidget {
         ),
         suffixIcon: GestureDetector(
           onTap: () {
-            //_startListening();
+            _startListening();
           },
           child: const Icon(
             Icons.mic,
