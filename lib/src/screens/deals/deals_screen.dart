cad1158cdc9fc9b760f1a0bebed41487c3df4554
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-12-24 08:38:10 +0100

    feat(deals): starting deals with smooth_page_indicator

--- lib/src/screens/deals/deals_screen.dart
+++ lib/src/screens/deals/deals_screen.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class DealsScreen extends StatelessWidget {
-  static const navigatorId = '/deals_screen';
+  final _controller = PageController();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text(
-        "Deals"
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[200],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          SizedBox(
+            height: 500,
+            child: PageView(
+              controller: _controller,
+              children: const [
+                Page1(),
+                Page2(),
+                Page3(),
+                Page4(),
+              ],
+            ),
+          ),
+
+          // dot indicators
+          SmoothPageIndicator(
+            controller: _controller,
+            count: 4,
+            effect: ExpandingDotsEffect(
+              activeDotColor: Colors.deepPurple,
+              dotColor: Colors.deepPurple.shade100,
+              dotHeight: 30,
+              dotWidth: 30,
+              spacing: 16,
+            ),
+          ),
+        ],
       ),
     );
   }
