f6c362157eb0d8b109eb7ee699f35f049b79039d
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-12-24 18:56:08 +0100

    feat(deals): add deals controller

---
+++ lib/src/controllers/deals/search_deals_controller.dart
@@ -0,0 +1,86 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:get/get.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:stud_advice/src/controllers/search/custom_search_controller.dart';
+import 'package:stud_advice/src/models/stud_advice/deals.dart';
+
+class SearchDealsController extends CustomSearchController {
+  final Dio _dio = Get.find();
+
+  final PagingController<int, DealContent> pagingController =
+  PagingController(firstPageKey: 0);
+
+  @override
+  void onInit() {
+    super.onInit();
+    pagingController.addPageRequestListener((pageKey) {
+      fetchPage(pageKey);
+    });
+  }
+
+  @override
+  Future<void> fetchPage(int pageKey) async {
+    try {
+      final newPage = await getDealsBySearch(
+        number: pageKey,
+        size: 5,
+        query: textEditingController.text,
+      );
+
+      final isLastPage = newPage.last;
+      final newItems = newPage.content;
+      if (newItems.isNotEmpty) {
+        if (isLastPage) {
+          pagingController.appendLastPage(newItems);
+        } else {
+          final nextPageKey = pageKey + 1;
+          pagingController.appendPage(newItems, nextPageKey);
+        }
+      } else {
+        pagingController.appendLastPage([]);
+      }
+    } catch (error) {
+      pagingController.error = error;
+    }
+  }
+
+  @override
+  void onClose() {
+    pagingController.dispose();
+    super.onClose();
+  }
+
+  @override
+  void refresh() {
+    pagingController.refresh();
+  }
+
+  Future<Deals> _getDealsBySearch(
+      String path, Map<String, dynamic> queryParameters) async {
+    try {
+      final response = await _dio.get(
+        path,
+        queryParameters: queryParameters,
+      );
+      if (response.statusCode == HttpStatus.ok) {
+        print(response.data);
+        return Deals.fromJson(response.data);
+      } else {
+        throw Exception('Failed to load categories');
+      }
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  Future<Deals> getDealsBySearch({
+    required int number,
+    required int size,
+    String? query,
+  }) async {
+    final queryParameters = {'page': number, 'size': size, 'searchText': query};
+    return _getDealsBySearch('/deals/search', queryParameters);
+  }
+}
