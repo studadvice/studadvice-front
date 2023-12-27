00f1c9ec316b0ac506b8d3335429097d60a93ddf
Churchill Atchedji <churchill.atchedji@limpidius.com>
2023-12-27 23:01:20 +0100

    feat(admin_process): continue improving administrative process style

--- lib/src/widgets/administrative_process/administrative_process_tile.dart
+++ lib/src/widgets/administrative_process/administrative_process_tile.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:get/get.dart';
 import 'package:stud_advice/stud_advice.dart';
 
@@ -15,6 +16,7 @@ class AdministrativeProcessListTile extends StatelessWidget {
   final AdministrativeProcessController _administrativeProcessController =
       Get.find<AdministrativeProcessController>();
   final FileController fileController = Get.find();
+  final RxBool isAnimated = true.obs;
 
   AdministrativeProcessListTile({
     Key? key,
@@ -34,109 +36,133 @@ class AdministrativeProcessListTile extends StatelessWidget {
       },
       child: Padding(
         padding: const EdgeInsets.all(2.0),
-        child: Container(
-          margin: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 8.0),
-          constraints: BoxConstraints(
-            maxWidth: MediaQuery.of(context).size.width - 16.0,
-          ),
-          decoration: BoxDecoration(
-            color: AppColors.white,
-            borderRadius: BorderRadius.circular(15.0),
-            boxShadow: const [
-              BoxShadow(
-                color: AppColors.black26,
-                offset: Offset(0, 2),
-                blurRadius: 4.0,
+        child: Obx(
+          () => SlideTransition(
+            position: Tween<Offset>(
+              begin: isAnimated.isTrue ? const Offset(-1, 0) : Offset.zero,
+              end: Offset.zero,
+            ).animate(CurvedAnimation(
+              curve: Curves.easeInOut,
+              parent: AnimationController(
+                vsync: TestVSync(),
+                duration: const Duration(milliseconds: 500),
+              )..forward(), // Start the animation
+            )),
+            child: Container(
+              margin:
+                  const EdgeInsets.symmetric(vertical: 1.0, horizontal: 8.0),
+              constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width - 16.0,
               ),
-            ],
-          ),
-          child: ListTile(
-            contentPadding: const EdgeInsets.all(8.0),
-            leading: ClipRRect(
-              borderRadius: BorderRadius.circular(15.0),
-              child: Container(
-                  padding: const EdgeInsets.all(5.0),
-                  child: FutureBuilder<String?>(
-                    future: fileController.downloadFile(imageId),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.done &&
-                          snapshot.data != null) {
-                        return Image.file(
-                          File(snapshot.data!),
-                          width: 50,
-                          height: 50,
-                        );
-                      } else if (snapshot.hasError) {
-                        return const Icon(
-                          Icons.error_outline_sharp,
-                          size: 50.0,
-                        );
-                      } else {
-                        return const Icon(Icons.image);
-                      }
-                    },
-                  )),
-            ),
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style: const TextStyle(
-                    fontSize: AppFontSizes.large18,
-                    fontWeight: FontWeight.w500,
+              decoration: BoxDecoration(
+                color: AppColors.white,
+                borderRadius: BorderRadius.circular(15.0),
+                boxShadow: const [
+                  BoxShadow(
+                    color: AppColors.black26,
+                    offset: Offset(0, 2),
+                    blurRadius: 4.0,
                   ),
+                ],
+              ),
+              child: ListTile(
+                contentPadding: const EdgeInsets.all(8.0),
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Container(
+                      padding: const EdgeInsets.all(5.0),
+                      child: FutureBuilder<String?>(
+                        future: fileController.downloadFile(imageId),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                                  ConnectionState.done &&
+                              snapshot.data != null) {
+                            return Image.file(
+                              File(snapshot.data!),
+                              width: 50,
+                              height: 50,
+                            );
+                          } else if (snapshot.hasError) {
+                            return const Icon(
+                              Icons.error_outline_sharp,
+                              size: 50.0,
+                            );
+                          } else {
+                            return const Icon(Icons.image);
+                          }
+                        },
+                      )),
                 ),
-                const SizedBox(height: 5.0),
-                Text(
-                  description,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  textAlign: TextAlign.start,
-                  style: const TextStyle(
-                    fontSize: AppFontSizes.medium,
-                    fontWeight: FontWeight.bold,
-                    color: AppColors.grey,
-                  ),
-                ),
-              ],
-            ),
-            subtitle: showProgressBar
-                ? const Padding(
-                    padding: EdgeInsets.only(top: 10.0),
-                    child: LinearProgressIndicator(
-                      value: 0.5,
-                      backgroundColor: AppColors.grey,
-                      valueColor: AlwaysStoppedAnimation<Color>(AppColors.blue),
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      name,
+                      style: const TextStyle(
+                        fontSize: AppFontSizes.large18,
+                        fontWeight: FontWeight.w500,
+                      ),
                     ),
-                  )
-                : null,
-            trailing: Obx(() {
-              return GestureDetector(
-                onTap: () {
-                  _administrativeProcessController
-                      .toggleFavoriteState(administrativeProcessId);
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(5.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    border: Border.all(color: AppColors.grey),
-                  ),
-                  child: Icon(
-                    Icons.favorite,
-                    color: _administrativeProcessController
-                            .isProcessFavorite(administrativeProcessId)
-                        ? AppColors.red
-                        : AppColors.black,
-                    size: 20.0,
-                  ),
+                    const SizedBox(height: 5.0),
+                    Text(
+                      description,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.start,
+                      style: const TextStyle(
+                        fontSize: AppFontSizes.medium,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.grey,
+                      ),
+                    ),
+                  ],
                 ),
-              );
-            }),
+                subtitle: showProgressBar
+                    ? const Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                        child: LinearProgressIndicator(
+                          value: 0.5,
+                          backgroundColor: AppColors.grey,
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(AppColors.blue),
+                        ),
+                      )
+                    : null,
+                trailing: Obx(() {
+                  return GestureDetector(
+                    onTap: () {
+                      _administrativeProcessController
+                          .toggleFavoriteState(administrativeProcessId);
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(5.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        border: Border.all(color: AppColors.grey),
+                      ),
+                      child: Icon(
+                        Icons.favorite,
+                        color: _administrativeProcessController
+                                .isProcessFavorite(administrativeProcessId)
+                            ? AppColors.red
+                            : AppColors.black,
+                        size: 20.0,
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            ),
           ),
         ),
       ),
     );
+  }
+}
+
+class TestVSync extends TickerProvider {
+  @override
+  Ticker createTicker(TickerCallback onTick) {
+    return Ticker(onTick);
   }
 }
