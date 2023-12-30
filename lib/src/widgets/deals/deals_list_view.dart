13b7f9c3c1ae010539ec04010fb82cdbf7c55dbe
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-12-30 11:10:02 +0100

    fix(deals): fix overflowed when no data

--- lib/src/widgets/deals/deals_list_view.dart
+++ lib/src/widgets/deals/deals_list_view.dart
@@ -19,122 +19,122 @@ class DealsListView extends StatelessWidget {
       builder: (controller) {
         if (controller.searchQuery.isEmpty) {
           return Scaffold(
-            body: FutureBuilder<Deals>(
-              future: dealsController.getDealsBySearch(size: 3, number: 0),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  if (snapshot.hasData && snapshot.data!.content.isNotEmpty) {
-                    final deals = snapshot.data!;
-                    return Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height / 4,
-                            child: PageView.builder(
-                              controller: _controller,
-                              itemCount: deals.numberOfElements,
-                              itemBuilder: (context, index) {
-                                return DealItemSlide(
-                                    deal: deals.content[index]);
-                              },
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          SmoothPageIndicator(
-                            controller: _controller,
-                            count: deals.content.length,
-                            effect: const ExpandingDotsEffect(
-                              activeDotColor: AppColors.primaryColor,
-                              dotColor: AppColors.grey,
-                              dotHeight: 10,
-                              dotWidth: 10,
-                              spacing: 10,
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                'deals.recommendation'.tr,
-                                style: const TextStyle(
-                                  fontSize: AppFontSizes.large20,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              GestureDetector(
-                                onTap: () {
-                                  Get.to(() => DealsSearchScreen());
+            body: SingleChildScrollView(
+              child: FutureBuilder<Deals>(
+                future: dealsController.getDealsBySearch(size: 3, number: 0),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasData && snapshot.data!.content.isNotEmpty) {
+                      final deals = snapshot.data!;
+                      return Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height / 4,
+                              child: PageView.builder(
+                                controller: _controller,
+                                itemCount: deals.numberOfElements,
+                                itemBuilder: (context, index) {
+                                  return DealItemSlide(
+                                      deal: deals.content[index]);
                                 },
-                                child: const Icon(
-                                  Icons.add,
-                                  color: AppColors.black,
-                                  size: 30,
-                                ),
                               ),
-                            ],
-                          ),
-                          FutureBuilder<Deals>(
-                            future: dealsController.getRecommendedDeals(
-                                size: 4, number: 0),
-                            builder: (context, recommendedSnapshot) {
-                              if (recommendedSnapshot.connectionState ==
-                                  ConnectionState.done) {
-                                if (recommendedSnapshot.hasData) {
-                                  final recommendedDeals =
-                                      recommendedSnapshot.data!;
-                                  return Expanded(
-                                    child: GridView.builder(
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            SmoothPageIndicator(
+                              controller: _controller,
+                              count: deals.content.length,
+                              effect: const ExpandingDotsEffect(
+                                activeDotColor: AppColors.primaryColor,
+                                dotColor: AppColors.grey,
+                                dotHeight: 10,
+                                dotWidth: 10,
+                                spacing: 10,
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'deals.recommendation'.tr,
+                                  style: const TextStyle(
+                                    fontSize: AppFontSizes.large20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    Get.to(() => DealsSearchScreen());
+                                  },
+                                  child: const Icon(
+                                    Icons.add,
+                                    color: AppColors.black,
+                                    size: 30,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            FutureBuilder<Deals>(
+                              future: dealsController.getRecommendedDeals(
+                                  size: 4, number: 0),
+                              builder: (context, recommendedSnapshot) {
+                                if (recommendedSnapshot.connectionState ==
+                                    ConnectionState.done) {
+                                  if (recommendedSnapshot.hasData) {
+                                    final recommendedDeals =
+                                    recommendedSnapshot.data!;
+                                    return GridView.builder(
                                       shrinkWrap: true,
                                       physics:
-                                          const AlwaysScrollableScrollPhysics(),
+                                      const AlwaysScrollableScrollPhysics(),
                                       gridDelegate:
-                                          const SliverGridDelegateWithFixedCrossAxisCount(
+                                      const SliverGridDelegateWithFixedCrossAxisCount(
                                         crossAxisCount: 2,
                                         crossAxisSpacing: 10,
                                         mainAxisSpacing: 10,
                                       ),
                                       itemCount:
-                                          recommendedDeals.numberOfElements,
+                                      recommendedDeals.numberOfElements,
                                       itemBuilder: (context, index) {
                                         return DealItem(
                                           deal: recommendedDeals.content[index],
                                         );
                                       },
-                                    ),
-                                  );
+                                    );
+                                  } else {
+                                    return NoResultIndicator();
+                                  }
                                 } else {
-                                  return NoResultIndicator();
+                                  return const Center(
+                                    child: CircularProgressIndicator(),
+                                  );
                                 }
-                              } else {
-                                return const Center(
-                                  child: CircularProgressIndicator(),
-                                );
-                              }
-                            },
-                          ),
-                        ],
-                      ),
-                    );
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    } else {
+                      return NoResultIndicator();
+                    }
+                  } else if (snapshot.hasError) {
+                    return const Icon(Icons.image);
                   } else {
-                    return NoResultIndicator();
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
                   }
-                } else if (snapshot.hasError) {
-                  return const Icon(Icons.image);
-                } else {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              },
+                },
+              ),
             ),
           );
         }
         return RefreshIndicator(
           onRefresh: () => Future.sync(
-            () => controller.pagingController.refresh(),
+                () => controller.pagingController.refresh(),
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
