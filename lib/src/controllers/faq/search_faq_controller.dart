26a5ce89073d3166c1b94da204c1343631b792d1
Nada Zine <nada.zine.etu@univ-lille.fr>
2023-12-31 17:02:24 +0100

    feat(faq): add faq

---
+++ lib/src/controllers/faq/search_faq_controller.dart
@@ -0,0 +1,88 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:get/get.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:stud_advice/src/controllers/search/custom_search_controller.dart';
+import 'package:stud_advice/src/models/stud_advice/categories.dart';
+
+import '../../models/stud_advice/faq.dart';
+
+class SearchFaqController extends CustomSearchController {
+  final Dio _dio = Get.find();
+
+  final PagingController<int, FaqContent> pagingController =
+  PagingController(firstPageKey: 0);
+
+  @override
+  void onInit() {
+    super.onInit();
+    pagingController.addPageRequestListener((pageKey) {
+      fetchPage(pageKey);
+    });
+  }
+
+  @override
+  Future<void> fetchPage(int pageKey) async {
+    try {
+      final newPage = await getFaqBySearch(
+        number: pageKey,
+        size: 5,
+        query: searchQuery.value,
+      );
+
+      final isLastPage = newPage.last;
+      final newItems = newPage.content;
+      if (newItems.isNotEmpty) {
+        if (isLastPage) {
+          pagingController.appendLastPage(newItems);
+        } else {
+          final nextPageKey = pageKey + 1;
+          pagingController.appendPage(newItems, nextPageKey);
+        }
+      } else {
+        pagingController.appendLastPage([]);
+      }
+    } catch (error) {
+      pagingController.error = error;
+    }
+  }
+
+  @override
+  void onClose() {
+    pagingController.dispose();
+    super.onClose();
+  }
+
+  @override
+  void refresh() {
+    pagingController.refresh();
+  }
+
+  Future<Faq> _getFaqBySearch(
+      String path, Map<String, dynamic> queryParameters) async {
+    try {
+      final response = await _dio.get(
+        path,
+        queryParameters: queryParameters,
+      );
+
+      if (response.statusCode == HttpStatus.ok) {
+        return Faq.fromJson(response.data);
+      } else {
+        throw Exception('Failed to load faq');
+      }
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  Future<Faq> getFaqBySearch({
+    required int number,
+    required int size,
+    String? query,
+  }) async {
+    final queryParameters = {'page': number, 'size': size, 'searchText': query};
+    return _getFaqBySearch('/faq/search', queryParameters);
+  }
+}
