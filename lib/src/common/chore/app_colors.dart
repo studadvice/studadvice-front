4e885d555a151bd9e49497abb36ac1eb35e21ba5
Nada Zine <nada.zine.etu@univ-lille.fr>
2024-01-04 20:26:47 +0100

    style(theme): change primary color

--- lib/src/common/chore/app_colors.dart
+++ lib/src/common/chore/app_colors.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   // TODO : remove this block
-  static const primaryColor = Color(0xFF3D5EE5);
+  static const primaryColor = Color(0xFF219EBC);
   static const primaryColorAccent = Color(0xFF219EBC);
-  static const secondaryColor = Color(0xFF023047);
+  static const secondaryColor = Color(0xFFFC8500);
   static const accentColor = Color(0xFFFFB703);
   static const accentColorAccent = Color(0xFFFB8500);
   static const dangerColor = Color(0xFFD62828);
