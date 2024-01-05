623ddc2cddd58b3fff7a3b460a2c311e2b1b27f6
Nada Zine <nada.zine.etu@univ-lille.fr>
2024-01-05 09:29:49 +0100

    feat(roadmap): add roadmap classes

--- lib/src/widgets/common/paints/step_detail/step_detail_card_widget.dart
+++ lib/src/widgets/common/paints/step_detail/step_detail_card_widget.dart
@@ -6,6 +6,7 @@ class StepDetailCardWidget extends StatelessWidget {
   final String description;
   final VoidCallback? onCompletePressed;
   final VoidCallback? onNextPressed;
+  final String actionText;
   final Color? cardColor;
   final double? cardRadius;
 
@@ -13,6 +14,7 @@ class StepDetailCardWidget extends StatelessWidget {
     super.key,
     required this.title,
     required this.description,
+    this.actionText = 'Action',
     this.cardColor,
     this.cardRadius,
     this.onCompletePressed,
@@ -22,42 +24,29 @@ class StepDetailCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          buildDragHandler(),
-          Text(
-            title,
-            style: const TextStyle(
-              fontSize: 22.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 18.0),
-          Expanded(
-            child: SingleChildScrollView( 
-              child: Text(
-                description,
-                style: const TextStyle(
-                  fontSize: 16.0,
-                ),
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            buildDragHandler(),
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+                color: AppColors.black,
               ),
             ),
-          ),
-          const SizedBox(height: 20.0),
-          // reinit the process
-          if (onCompletePressed != null)
-            CustomButton(
-              text: 'Recommençer',
-              onPressed: onCompletePressed!,
-              textColor: AppColors.white,
-              backgroundColor: AppColors.primaryColorAccent,
-              horizontalPadding: 30,
-              verticalPadding: 8,
+            const SizedBox(height: 18.0),
+            Text(
+              description,
+              style: const TextStyle(
+                fontSize: 16.0,
+                color: AppColors.black,
+              ),
             ),
-        ],
+            const Spacer()],
+
       ),
     );
   }
