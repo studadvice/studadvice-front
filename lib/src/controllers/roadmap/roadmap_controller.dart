77634d412ec11a8202366473c3a96c48f22013ea
Nada Zine <nada.zine.etu@univ-lille.fr>
2024-01-05 09:29:49 +0100

    feat(roadmap): add roadmap classes

--- lib/src/controllers/roadmap/roadmap_controller.dart
+++ lib/src/controllers/roadmap/roadmap_controller.dart
@@ -1,16 +1,9 @@
-import 'dart:io';
-import 'dart:math';
-
-import 'package:dio/dio.dart';
-import 'package:english_words/english_words.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:stud_advice/stud_advice.dart';
-import 'package:stud_advice/src/controllers/search/custom_search_controller.dart';
 
+class RoadMapController extends GetxController {
+  var currentStep = 0.obs;
 
-class RoadMapController extends CustomSearchController {
-
-  final _dio = Get.find<Dio>();
-  
+  void changeStep(int index) {
+    currentStep.value = index;
+  }
 }
