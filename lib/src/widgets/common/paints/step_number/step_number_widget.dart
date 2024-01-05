cbad7e5e5f62fa679f60ca0b09cc2889f0ec209f
Nada Zine <nada.zine.etu@univ-lille.fr>
2024-01-05 09:29:49 +0100

    feat(roadmap): add roadmap classes

--- lib/src/widgets/common/paints/step_number/step_number_widget.dart
+++ lib/src/widgets/common/paints/step_number/step_number_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stud_advice/src/widgets/common/paints/step_number/step_number_painter.dart';
 import 'package:stud_advice/stud_advice.dart';
 
 class StepNumberWidget extends StatelessWidget {
@@ -6,10 +7,8 @@ class StepNumberWidget extends StatelessWidget {
   final double diameter;
   final Color color;
   final bool isActivated;
-  final Color borderColor;
   final Widget? child;
   final VoidCallback? onPressed;
-  final bool isLeftAligned;
 
   const StepNumberWidget({
     super.key,
@@ -17,52 +16,34 @@ class StepNumberWidget extends StatelessWidget {
     required this.diameter,
     required this.color,
     this.isActivated = false,
-    this.isLeftAligned = false,
     this.child,
     this.onPressed,
-    this.borderColor = const Color(0xFF219EBC),
   });
 
   @override
   Widget build(BuildContext context) {
     double borderWidth = isActivated ? 2.0 : 1.0;
-    Color displayColor = color;
+    Color displayColor = isActivated ? color : Colors.grey.withOpacity(0.5); // Use semi-transparency for non-activated steps
 
-    Widget stepNumberWidget = GestureDetector(
-      onTap: isActivated ? onPressed : null,
-      child: SizedBox(
-        width: diameter,
-        height: diameter,
-        child: Center(
-          child: CustomPaint(
-            painter: StepNumberPainter(
-              borderColor: borderColor,
-              stepNumber: stepNumber,
-              color: displayColor,
-              diameter: diameter,
-              borderWidth: borderWidth,
-              isActivated: isActivated,
+    return GestureDetector(
+      onTap: onPressed,
+      child: IntrinsicWidth(
+        child: SizedBox(
+          width: diameter,
+          height: diameter,
+          child: Center(
+            child: CustomPaint(
+              painter: StepNumberPainter(
+                stepNumber: stepNumber,
+                color: displayColor,
+                diameter: diameter,
+                borderWidth: borderWidth,
+                isActivated: isActivated,
+              ),
+              size: Size(diameter, diameter),
             ),
-            size: Size(diameter, diameter),
           ),
         ),
-      ),
-    );
-
-    return IntrinsicWidth(
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          if (isLeftAligned) ...[
-            stepNumberWidget,
-            const SizedBox(width: 16),
-            Expanded(child: child!),
-          ] else ...[
-            Expanded(child: child!),
-            const SizedBox(width: 16),
-            stepNumberWidget,
-          ],
-        ],
       ),
     );
   }
