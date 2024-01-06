0c7023383652b04f2a252f63b9b8f58d10a8fd95
Nada Zine <nada.zine.etu@univ-lille.fr>
2024-01-06 20:59:00 +0100

    feat(administrative-process): differentiate between financial aid and others

--- lib/src/widgets/administrative_process/administrative_process_tile.dart
+++ lib/src/widgets/administrative_process/administrative_process_tile.dart
@@ -14,6 +14,7 @@ class AdministrativeProcessListTile extends StatelessWidget {
   final Color backgroundColor;
   final List<StepItem> steps;
   final String type;
+  final String type;
 
   final AdministrativeProcessController _administrativeProcessController =
   Get.find<AdministrativeProcessController>();
